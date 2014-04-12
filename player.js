643ad8adcdf5872ab0cd77d98f986e0862031b08
xza <xza@localhost>
2014-04-12 11:16:15 +0200

    v 0.0.2

--- player.js
+++ player.js
@@ -3,17 +3,18 @@ function weArePreflop(game)
     return game.community_cards === undefined || game.community_cards.length < 1;
 }
 module.exports = {
+    
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "Default JavaScript folding player V 0.0.2",
 
   bet_request: function(game_state) {
       var game = JSON.parse(game_state);
+      var me = game.players[game.in_action];
       if (weArePreflop(game))
       {
-          var me = game.players[game.in_action];
           return game.current_buy_in - me.bet + 3 * game.minumum_raise;
       }
-    return 0;
+    return 50;
   },
 
   showdown: function(game_state) {
