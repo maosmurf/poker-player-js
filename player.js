f878b074e88ad2650287ec3c791b66743098a1fa
xza <xza@localhost>
2014-04-12 11:25:11 +0200

    v 0.0.3 coolcards

--- player.js
+++ player.js
@@ -2,19 +2,40 @@ function weArePreflop(game)
 {
     return game.community_cards === undefined || game.community_cards.length < 1;
 }
+function countCoolCard(holeCards)
+{
+    var coolCards = {J: 'J', Q: 'Q', K: 'K', A: 'A'};
+
+    var myCoolCards = 0;
+
+    holeCards.forEach(function (card)
+    {
+        for(var coolCard in coolCards) {
+            if (coolCard == card) {
+                myCoolCards++;
+            }
+        }
+    });
+
+    return myCoolCards;
+}
 module.exports = {
     
 
-  VERSION: "Default JavaScript folding player V 0.0.2",
+  VERSION: "Default JavaScript folding player V 0.0.3",
 
   bet_request: function(game_state) {
       var game = JSON.parse(game_state);
       var me = game.players[game.in_action];
+      var holeCards = me.hole_cards;
       if (weArePreflop(game))
       {
           return game.current_buy_in - me.bet + 3 * game.minumum_raise;
       }
-    return 50;
+      if (countCoolCard(holeCards) == 2) {
+          return game.current_buy_in - me.bet + 8 * game.minumum_raise;
+      }
+      return 0;
   },
 
   showdown: function(game_state) {
