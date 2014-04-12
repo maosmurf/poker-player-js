6110f4e8604dc1336c291619062424c178f4af34
Nik Graf <nik@blossom.io>
2014-04-12 15:38:57 +0200

    added test for cool cards

--- test.js
+++ test.js
@@ -4,3 +4,21 @@ exports.testHelp = function(test){
     test.equals(helper.help(), 0);
     test.done();
 };
+
+exports.testCountCoolCard = function(test){
+    cards = [{
+            "rank": "10",
+            "suit": "hearts"
+        },
+        {
+            "rank": "7",
+            "suit": "spades"
+        }
+    ]
+    test.equals(helper.countCoolCard(cards), 1);
+    test.done();
+};
+
+
+
+
