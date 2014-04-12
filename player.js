cecb183b7f5c9640ddab1fb750191926c2a5f19e
xza <xza@localhost>
2014-04-12 13:16:48 +0200

    v10

--- player.js
+++ player.js
@@ -1,8 +1,14 @@
+// We need this to build our post string
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+var httpsync = require('httpsync');
+
 function weArePreflop(game)
 {
     return game.community_cards === undefined || game.community_cards.length < 1;
 }
-function weHavePairs(holeCards)
+function weHavePairsHoleCards(holeCards)
 {
     return (holeCards[0] === holeCards[1]);
 }
@@ -28,30 +34,73 @@ function raiseAmount(game, me, raiseFactor)
 {
     var currentBuyIn = game.current_buy_in;
     var bet = me.bet;
-    var minumumRaise = game.minimum_raise;
-    return currentBuyIn - bet + raiseFactor * minumumRaise;
+    var minimumRaise = game.minimum_raise;
+    return currentBuyIn - bet + raiseFactor * minimumRaise;
 }
+//function getRank(game)
+//{
+//    var allCards = holeCards.concat(game.community_cards);
+//
+//    httpsync.request({
+//        url: "http://localhost:2048/",
+//          method: "POST"
+//    });
+//
+//    var post_data = querystring.stringify({
+//            'cards' : allCards
+//      });
+//
+//    var post_options = {
+//          host: 'localhost',
+//          port: '2048',
+//          path: '/',
+//          method: 'POST',
+//          headers: {
+//              'Content-Type': 'application/x-www-form-urlencoded',
+//              'Content-Length': post_data.length
+//          }
+//      };
+//
+//    var rank = -1;
+//    var post_req = http.request(post_options, function(res) {
+//          res.setEncoding('utf8');
+//          res.on('data', function (chunk) {
+//              rank = data.
+//          });
+//      });
+//
+//    post_req.write(post_data);
+//      post_req.end();
+//
+//}
 module.exports = {
 
-  VERSION: "Default JavaScript folding player V9",
+  VERSION: "Default JavaScript folding player V10",
 
   bet_request: function(game_state) {
+
       var game = JSON.parse(game_state);
       var me = game.players[game.in_action];
-      var holeCards = me.hole_cards;
-      if (weHavePairs(holeCards)) {
-          return  raiseAmount(game, me, 8);
+      if (weArePreflop(game)) {
+          if (weHavePairsHoleCards(me.hole_cards)) {
+              return  raiseAmount(game, me, 8);
+          }
+          if (countCoolCard(me.hole_cards) == 2) {
+              return raiseAmount(game, me, 8);
+          }
+          if (countCoolCard(me.hole_cards) == 1) {
+              return raiseAmount(game, me, 5);
+          }
+          return 0;
       }
-      if (countCoolCard(holeCards) == 2) {
-          return raiseAmount(game, me, 8);
+      if (weHavePairsHoleCards(me.hole_cards)) {
+          return  raiseAmount(game, me, 2);
       }
-      if (countCoolCard(holeCards) == 1) {
-          return raiseAmount(game, me, 5);
+      if (countCoolCard(me.hole_cards) == 2) {
+          return raiseAmount(game, me, 1);
       }
-      return raiseAmount(game, me, 3);
-//      if (weArePreflop(game))
-//      {
-//      }
+//      var rank = getRank(game);
+
       return 0;
   },
 
@@ -60,3 +109,4 @@ module.exports = {
 
   }
 };
+
