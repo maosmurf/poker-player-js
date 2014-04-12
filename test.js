1e177cda823cfafa834384ab14639f01a290d2e6
Nik Graf <nik@blossom.io>
2014-04-12 16:06:38 +0200

    added player count test

--- test.js
+++ test.js
@@ -20,5 +20,53 @@ exports.testCountCoolCard = function(test){
 };
 
 
+exports.testPlayersQuery = function(test){
+    players = [
+        {
+
+            "id": 0,
+
+            "name": "Albert",
+            "status": "active",
+            "version": "Default random player",
+
+            "stack": 1010,
+
+
+            "bet": 320
+        },
+        {
+            "id": 1,
+            "name": "Bob",
+            "status": "active",
+            "version": "Default random player",
+            "stack": 1590,
+            "bet": 80,
+            "hole_cards": [
+
+                {
+                    "rank": "10",
+                    "suit": "hearts"
+                },
+                {
+                    "rank": "7",
+                    "suit": "spades"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Chuck",
+            "status": "out",
+            "version": "Default random player",
+            "stack": 0,
+            "bet": 0
+        }
+    ]
+    test.equals(helper.countActivePlayers(players), 2);
+    test.done();
+};
+
+
 
 
